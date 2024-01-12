66249d15a7fbe8cac675dc3f6bc5f834143340e1
matheusGolmini <matheusgolminif@gmail.com>
2024-01-12 11:43:54 -0300

    feat: add new integration tests

--- src/test/java/tech/challenge/account/service/account/application/gateway/ClienteGatewayIT.java
+++ src/test/java/tech/challenge/account/service/account/application/gateway/ClienteGatewayIT.java
@@ -6,12 +6,17 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import tech.challenge.account.service.account.domain.entities.Cliente;
+import tech.challenge.account.service.account.domain.valueObjects.CPF;
+import tech.challenge.account.service.account.domain.valueObjects.Email;
+import tech.challenge.account.service.account.helpers.ClienteHelper;
 import tech.challenge.account.service.account.infrastructure.db.repositories.ClienteRepository;
 
 import java.util.List;
+import java.util.UUID;
 
+import static java.text.MessageFormat.format;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @AutoConfigureTestDatabase
@@ -39,4 +44,76 @@ public class ClienteGatewayIT {
         assertEquals(5, resultado.size());
     }
 
+    @Test
+    void buscarClientePorCPF_DeveRetornarOCliente() {
+        // Arrange
+        var cpf = new CPF("831.643.420-20");
+
+        // Act
+        var cliente = clienteGateway.buscarClientePorCpf(cpf);
+
+        // Assert
+        assertEquals(cliente.getEmail().getValue(), "Vick@gmail.com");
+        assertEquals(cliente.getCpf().getValue(), cpf.getValue());
+    }
+
+    @Test
+    void buscarClientePorCPF_DeveRetornarExcecaoQuandoOClienteNaoForEncontrado() {
+        // Arrange
+        var cpf = new CPF("697.525.870-89");
+
+        // Act / Assert
+        Exception exception = assertThrows(RuntimeException.class, () -> clienteGateway.buscarClientePorCpf(cpf));
+        assertEquals(format("Registro não encontrado com cpf {0}",cpf.getValue()), exception.getMessage());
+    }
+
+    @Test
+    void buscarClientePorEmail_DeveRetornarOCliente() {
+        // Arrange
+        var email = new Email("Adam@gmail.com");
+
+        // Act
+        var cliente = clienteGateway.buscarClientePorEmail(email);
+
+        // Assert
+        assertEquals(cliente.getCpf().getValue(), "092.420.830-97");
+        assertEquals(cliente.getEmail().getValue(), email.getValue());
+    }
+
+    @Test
+    void buscarClientePorEmail_DeveRetornarExcecaoQuandoOClienteNaoForEncontrado() {
+        // Arrange
+        var email = new Email("fake@gmail.com");
+
+        // Act / Assert
+        Exception exception = assertThrows(RuntimeException.class, () -> clienteGateway.buscarClientePorEmail(email));
+        assertEquals(format("Registro não encontrado com email {0}",email.getValue()), exception.getMessage());
+    }
+
+    @Test
+    void cadastrarCliente_DeveCadastrarOCliente() {
+        // Arrange
+        var cliente = ClienteHelper.gerarCliente();
+
+        // Act
+        clienteGateway.cadastrarCliente(cliente);
+        var result = clienteGateway.buscarClientePorCpf(cliente.getCpf());
+
+        // Assert
+        assertEquals(result.getCpf().getValue(), cliente.getCpf().getValue());
+        assertEquals(result.getNome(), cliente.getNome());
+    }
+
+    @Test
+    void deletarClientePorId_DeveDeletarOCliente() {
+        // Arrange
+        var id = UUID.fromString("592ac344-9f12-40cd-8ed9-1fde6ad9006e");
+
+        // Act
+        clienteGateway.deletarCliente(id);
+        var cliente = clienteRepository.findById(id);
+
+        // Assert
+        assertTrue(cliente.isEmpty());
+    }
 }
