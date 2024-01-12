a6ceb5b0588c083fce19ee4996dd88a1c0a72b5b
matheusGolmini <matheusgolminif@gmail.com>
2024-01-12 09:16:07 -0300

    feat: add integration test

--- src/test/java/tech/challenge/account/service/account/application/gateway/ClienteGatewayIT.java
+++ src/test/java/tech/challenge/account/service/account/application/gateway/ClienteGatewayIT.java
@@ -1,4 +1,42 @@
-//package tech.challenge.account.service.account.application.gateway;
-//
-//public class ClienteGatewayIT {
-//}
+package tech.challenge.account.service.account.application.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import tech.challenge.account.service.account.domain.entities.Cliente;
+import tech.challenge.account.service.account.infrastructure.db.repositories.ClienteRepository;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@AutoConfigureTestDatabase
+@Transactional
+public class ClienteGatewayIT {
+
+    @Autowired
+    private ClienteGateway clienteGateway;
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @Test
+    void devePermitirCriarTabela() {
+        var totalDeRegistros = clienteRepository.count();
+        assertThat(totalDeRegistros).isNotNegative();
+    }
+
+    @Test
+    void buscarClientes_DeveRetornarListaDeClientes() {
+        // Act
+        List<Cliente> resultado = clienteGateway.buscarClientes();
+
+        // Assert
+        assertEquals(5, resultado.size());
+    }
+
+}
