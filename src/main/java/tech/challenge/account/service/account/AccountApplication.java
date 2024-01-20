104cf28c5b1258115ffc9ca965fe70f3f74add3b
matheusGolmini <matheusgolminif@gmail.com>
2024-01-20 14:41:35 -0300

    feat: config swagger

--- src/main/java/tech/challenge/account/service/account/AccountApplication.java
+++ src/main/java/tech/challenge/account/service/account/AccountApplication.java
@@ -1,9 +1,16 @@
 package tech.challenge.account.service.account;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Tech Challenge - Account Service", version = "1.0"))
+@SecurityScheme(name = "javainuseapi", scheme = "basic", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
 public class AccountApplication {
 
     public static void main(String[] args) {
